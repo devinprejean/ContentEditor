2d42596a69895a2a59864e9718fb632e2f7ab608
Prejean, Devin J <prejedj@cpchem.com>
2018-10-23 13:08:45 -0500

    last minute changes for 1.6.0 support. builds, packages, and installs successfully.

--- src/webparts/scriptEditor/components/ScriptEditor.module.scss.js
+++ src/webparts/scriptEditor/components/ScriptEditor.module.scss.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /* tslint:disable */
 require('./ScriptEditor.module.css');
 var styles = {
-    scriptEditor: 'scriptEditor_573ef9f6',
-    container: 'container_573ef9f6',
-    row: 'row_573ef9f6',
+    scriptEditor: 'scriptEditor_b61a766d',
+    container: 'container_b61a766d',
+    row: 'row_b61a766d',
 };
 exports.default = styles;
 /* tslint:enable */ 
